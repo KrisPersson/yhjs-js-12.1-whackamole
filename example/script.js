09081d6a0bba639b930fa89c3710ee6b567cb760
KrisPersson <krisperssonmusic@gmail.com>
2022-11-23 16:13:11 +0100

    Added features

--- example/script.js
+++ example/script.js
@@ -1,7 +1,43 @@
-const timeBetweenPopUps = 2000;
-const appearanceTime = 800;
-let currentHole = null;
-let molesWhacked = 0;
+const allHoles = document.querySelectorAll('main article');
+const bodyEl = document.querySelector('body')
+
+
+const gameState = {
+    timeBetweenPopUps: 2000,
+    appearanceTime: 800,
+    totalTime: 60,
+    currentTime: 0,
+    currentHole: null,
+    molesWhacked: 0,
+    playerName: '',
+    highscore: [
+        {
+         name: 'Sven-Dennis',
+         points: 20
+        },
+        {
+         name: 'Ada',
+         points: 18
+        },
+        {
+         name: 'Gunlög',
+         points: 15
+        },
+        {
+         name: 'Zlatan',
+         points: 12
+        },
+        {
+         name: 'Ove Molin',
+         points: 11
+        },
+        {
+         name: 'Lewis E Carroll',
+         points: 10
+        }
+     ]
+}
+
 // Append listener
 allHoles.forEach(hole => {
     hole.addEventListener('click', clickHandler)
@@ -10,31 +46,29 @@ allHoles.forEach(hole => {
 function clickHandler(e) {
 
     let whackedHole = e.target.getAttribute('data-id');
-    if(parseInt(whackedHole) === currentHole) {
+    if(parseInt(whackedHole) === gameState.currentHole) {
         // Hit!
         // +1 on score
-        molesWhacked++
+        gameState.molesWhacked++
 
         // Update whacked moles gui
-        document.querySelector('.moleswhacked b').innerHTML = molesWhacked;
+        document.querySelector('.moleswhacked b').innerHTML = gameState.molesWhacked;
 
         // add class to show hit
         document.querySelector(`[data-id="${whackedHole}"]`).classList.add('hit');
         setTimeout(() => {
             document.querySelector(`[data-id="${whackedHole}"]`).classList.remove('hit');
-        }, appearanceTime);
+        }, gameState.appearanceTime);
     }
 
 }
 
-
-
 function startGame() {
 
-    let currentTime = 60;
-    let hits = 0;
-    const allHoles = document.querySelectorAll('main article');
-    
+    gameState.molesWhacked = 0
+    gameState.currentHole = null
+    gameState.currentTime = gameState.totalTime
+    renderHighscore()
 
     let gameLoop = setInterval(() => {
 
@@ -46,7 +80,7 @@ function startGame() {
         let randomId = Math.floor(Math.random() * allHoles.length);
         
         // register as current Hole
-        currentHole = randomId;
+        gameState.currentHole = randomId;
 
         let el = document.querySelector(`[data-id="${randomId}"]`);
         el.classList.add('mole');
@@ -55,33 +89,146 @@ function startGame() {
         setTimeout(() => {
 
             allHoles.forEach(hole => hole.classList.remove('mole'));
-            currentHole = null
+            gameState.currentHole = null
 
-        }, appearanceTime)
-    }, timeBetweenPopUps);
+        }, gameState.appearanceTime)
+    }, gameState.timeBetweenPopUps);
 
 
     let timer = setInterval(() => {
         // Check if time left
-        if(currentTime >= 0) {
+        if(gameState.currentTime >= 0) {
 
         // Update timer in gui
-        document.querySelector('.timeleft b').innerHTML = `${currentTime}s`;
+        document.querySelector('.timeleft b').innerHTML = `${gameState.currentTime}s`;
 
         // count down current time
-        currentTime--;
+        gameState.currentTime--;
 
     } else {
         // Game over
         clearInterval(timer)
         clearInterval(gameLoop)
 
-        alert(`You whacked ${molesWhacked} moles in 60 sec.`)
+    
+        alert(`You whacked ${gameState.molesWhacked} moles in ${gameState.totalTime} sec.`)
+        submitHighscore(gameState.playerName, gameState.molesWhacked)
+        renderHighscore()
+        renderGameEndScreen()
+        
     }
     }, 1000);
 
 
 }
 
-startGame()
+function submitHighscore(name, score) {
+    let newArr = [...gameState.highscore]
 
+    if (newArr.length < 10) {
+        newArr.push({
+            name: name,
+            points: score
+        })
+    } else if (score > newArr[newArr.length - 1].points) {
+        newArr.pop()
+        newArr.push({
+            name: name,
+            points: score
+        })
+    }
+    gameState.highscore = sortHighscore(newArr)
+}
+
+function sortHighscore(arr) {
+    let sortedArr = arr.sort((a, b) => {
+        return b.points - a.points
+    })
+    return sortedArr
+}
+
+function renderHighscore() {
+
+    let htmlToBeRendered = ``
+
+    for (let i = 0; i < 10; i++) {
+        if (i < gameState.highscore.length) {
+            htmlToBeRendered += `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${gameState.highscore[i].name}</td>
+                <td>${gameState.highscore[i].points}</td>
+            </tr>
+            `
+        } else {
+            htmlToBeRendered += `
+            <tr>
+                <td>${1 + i}</td>
+                <td></td>
+                <td>0</td>
+            </tr>
+            `
+        }
+    }
+    document.querySelector('.highscore-board tbody').innerHTML = htmlToBeRendered
+}
+
+function renderStartScreen() {
+    const startScreenEl = document.createElement('aside')
+    startScreenEl.setAttribute('class', 'start-screen')
+    startScreenEl.innerHTML = `
+        <section class='start-screen__board'>
+            <p class="board__text">Enter name and select time-limit</p>
+            <input class="name-input" type="text" placeholder="Player name" >
+            <input class="time-input" type='number' id=“time” min='5' max='60' value='60' />
+            <button class="start-game-btn">Play</button>
+        </section>
+    `
+
+    bodyEl.appendChild(startScreenEl)
+
+    document.querySelector('.start-game-btn').addEventListener('click', handleClickStart)
+}
+
+function renderGameEndScreen() {
+    const endScreenEl = document.createElement('aside')
+    endScreenEl.setAttribute('class', 'end-screen')
+    endScreenEl.innerHTML = `
+        <section class='start-screen__board'>
+            <button class="play-again-btn">Play again</button>
+            <button class="start-screen-btn">Change player</button>
+        </section>
+    `
+    bodyEl.appendChild(endScreenEl)
+    document.querySelector('.play-again-btn').addEventListener('click', handleClickStart)
+    document.querySelector('.start-screen-btn').addEventListener('click', renderStartScreen)
+
+}
+
+function handleClickStart() {
+    const startScreenEl = document.querySelector('.start-screen')
+    const endScreenEl = document.querySelector('.end-screen')
+
+    const startScreenInput = document.querySelector('.name-input')
+    const numberInput = document.querySelector('.time-input')
+          
+    if (startScreenInput !== null) {
+        gameState.playerName = startScreenInput.value
+        gameState.totalTime = Number(numberInput.value)
+    }
+    
+    if (startScreenEl !== null) {
+        document.querySelector('.start-game-btn').removeEventListener('click', handleClickStart)
+        bodyEl.removeChild(startScreenEl)
+    }
+    if (endScreenEl !== null) {
+        document.querySelector('.play-again-btn').removeEventListener('click', handleClickStart)
+        document.querySelector('.start-screen-btn').removeEventListener('click', renderStartScreen)
+
+        bodyEl.removeChild(endScreenEl)
+    }
+
+    startGame()
+}
+
+renderStartScreen()
